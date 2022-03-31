48fa7f1c379ddf7cb5536e8cf250b6bcc17f49b9
Eden Chan <edenchan717@gmail.com>
2022-03-31 09:03:14 -0400

    :art: NAND, AND

--- coq/basics/Basics.v
+++ coq/basics/Basics.v
@@ -18,9 +18,9 @@ Definition next_weekday (d:day) : day :=
   | sunday    => monday
   end.
 
-Compute (next_weekday friday).
+(* Compute (next_weekday friday). *)
 (* ==> monday : day *)
-Compute (next_weekday (next_weekday saturday)).
+(* Compute (next_weekday (next_weekday saturday)). *)
 (* ==> tuesday : day *)
 Example test_next_weekday:
   (next_weekday (next_weekday saturday)) = tuesday.
@@ -68,11 +68,16 @@ Proof. simpl. reflexivity. Qed.
 Example test_orb4: (orb false false) = false.
 Proof. simpl. reflexivity. Qed.
 
+
+(* Infix syntax for boolean operators *)
 Notation "x && y" := (andb x y).
 Notation "x || y" := (orb x y).
+
 Example test_orb5:  false || false || true = true.
 Proof. simpl. reflexivity. Qed.
 
+
+(* If then else syntax *)
 Definition negb' (b:bool) : bool :=
   if b then false
   else true.
@@ -82,3 +87,36 @@ Definition andb' (b1:bool) (b2:bool) : bool :=
 Definition orb' (b1:bool) (b2:bool) : bool :=
   if b1 then true
   else b2.
+
+Example test_andb1: (andb true true) = true. 
+Proof. simpl. reflexivity. Qed.  
+Example test_andb2: (andb true false) = false. 
+Proof. simpl. reflexivity. Qed.  
+Example test_andb3: (andb false true) = false. 
+Proof. simpl. reflexivity. Qed.  
+Example test_andb4: (andb false false) = false. 
+Proof. simpl. reflexivity. Qed.  
+
+
+(*  exercise 1 star *)
+Definition nandb (b1 : bool) (b2 : bool) : bool := 
+  (negb (andb b1 b2)). 
+Example test_nandb1:               (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.  
+Example test_nandb2:               (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.  
+Example test_nandb3:               (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.  
+Example test_nandb4:               (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.  
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  (andb b1 (andb b2 b3)).
+Example test_andb31:                 (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed. 
+Example test_andb32:                 (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed. 
+Example test_andb33:                 (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed. 
+Example test_andb34:                 (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed. 
