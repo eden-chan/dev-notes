8d3cc52d7c1a099556696b1dde64954422be97e0
Eden Chan <edenchan717@gmail.com>
2022-04-11 22:57:19 -0400

    :art: binary numbers, increment binary by 1, and bin_to_nat

--- coq/basics/Basics.v
+++ coq/basics/Basics.v
@@ -741,3 +741,36 @@ destruct b eqn:Eb.
   reflexivity.
 Qed.
 
+(* 3 stars *)
+Inductive bin : Type :=
+  | Z
+  | B0 (n : bin)
+  | B1 (n : bin).
+
+Fixpoint incr (m:bin) : bin := 
+  match m with 
+    | Z => B1 Z 
+    | B1 m' => B0 (incr m')
+    | B0 m' => B1 m' 
+  end. 
+
+
+
+Fixpoint bin_to_nat (m:bin) : nat
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. reflexivity. Qed. 
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. reflexivity. Qed. 
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. reflexivity. Qed. 
+
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+(* FILL IN HERE *) Admitted.
+Example test_bin_incr5 :
+        bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+(* FILL IN HERE *) Admitted.
+Example test_bin_incr6 :
+        bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+(* FILL IN HERE *) Admitted.
